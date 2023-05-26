9a222d11819bc57ecc8b7450e5117990f1d748ae
TI <ti@pmsm.local>
2023-05-26 14:09:17 -0300

    changedAtWork

--- src/memeGeneretor/main.js
+++ src/memeGeneretor/main.js
@@ -2,17 +2,20 @@ import React from "react";
 
 
 const main = () => {
+    const handClick = () =>{
+        return console.log("oi estou aqui")
+    }
     return (
         <main>
             <div className="main-container">
                 <div className="form-section">
-                    <form>
+                    <div className="form">
                         <div className="input-field">
                             <input type="text" placeholder="meme" />
                             <input type="text" placeholder="other meme" />
                         </div>
-                        <button className="btn">Get a new image</button>
-                    </form>
+                        <button onClick={handClick} className="btn">Get a new image</button>
+                    </div>
                 </div>
                 <div className="image-container">
                     <img src="./memeImage/memeOne.jpeg" alt="memePhoto" />
