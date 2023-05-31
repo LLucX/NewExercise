dd7c748b2d1e91f5c8fc4cac44da985857a74364
Lucas <goolucaskn@gmail.com>
2023-05-31 16:07:48 -0300

    changes

--- src/memeGeneretor/main.js
+++ src/memeGeneretor/main.js
@@ -2,17 +2,24 @@ import React from "react";
 
 
 const main = () => {
+
+    const whenClicked = ()=>{
+        return console.log("I was clicked")
+    }
+
+    console.log("I'm here")
+
     return (
         <main>
             <div className="main-container">
                 <div className="form-section">
-                    <form>
+                    <div className="form">
                         <div className="input-field">
                             <input type="text" placeholder="meme" />
                             <input type="text" placeholder="other meme" />
                         </div>
-                        <button className="btn">Get a new image</button>
-                    </form>
+                        <button onClick={whenClicked} className="btn">Get a new image</button>
+                    </div>
                 </div>
                 <div className="image-container">
                     <img src="./memeImage/memeOne.jpeg" alt="memePhoto" />
