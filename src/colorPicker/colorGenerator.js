be669fd91419b9d8561bcb7ef211c309a483acb9
TI <ti@pmsm.local>
2023-06-16 13:25:35 -0300

    changes

--- src/colorPicker/colorGenerator.js
+++ src/colorPicker/colorGenerator.js
@@ -4,21 +4,6 @@ import React, {useState} from "react";
 
 const ColorGenerator = (props) =>{
 
-    const [randomColor, setRandomColor] = useState("black");
-
-  function generateRandomColor() {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-
-    return `rgb(${red}, ${green}, ${blue})`;
-  }
-
-  function handleClick() {
-    const newRandomColor = generateRandomColor();
-    setRandomColor(newRandomColor);
-  }
-
         
 return(
             <div style={{backgroundColor:randomColor}} className="box-pallets">
--- src/colorPicker/colorPickerData.js
+++ src/colorPicker/colorPickerData.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+
+
+const [randomColor, setRandomColor] = useState("black");
+
+  function generateRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+  }
+
+  function handleClick() {
+    const newRandomColor = generateRandomColor();
+    setRandomColor(newRandomColor);
+  }
 
 
 const colorPaletes = [
     {
         name:"one",
-        style:"firt"
+        color:"baseColor"
     },
     {
         name:"two",
